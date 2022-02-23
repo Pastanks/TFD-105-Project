f265c44158db5b562e7e777d7a3f579691e125c8
Pastanks <gn02212656@gmail.com>
2022-02-23 21:13:52 +0800

    罐罐RWD

--- javascript/all.js
+++ javascript/all.js
@@ -14,13 +14,12 @@ let gotopop = document.getElementsByClassName("top-btn")[0];
 window.addEventListener('scroll', showTopBtn);
 function showTopBtn() {
     let topfar = window.pageYOffset;
-    console.log(topfar);
 if (topfar > 300) {
     gotopop.classList.add("on");
 } else {
     gotopop.classList.remove("on");
 }
-}
+    };
 gotop.addEventListener("click", function () {
     document.body.scrollTop = 0; // for safari
     document.documentElement.scrollTop = 0; // for chrom
