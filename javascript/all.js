bcb5d52b9dfa0786dfffe3b6902d37e5cec58a06
Pastanks <gn02212656@gmail.com>
2022-02-23 19:57:17 +0800

    gotop

--- javascript/all.js
+++ javascript/all.js
@@ -8,3 +8,20 @@ let burger = document.getElementsByClassName("burger_btn");
     burgeron.classList.toggle("-on");
 });
 }
+// 回到頁首
+let gotop = document.getElementById("top");
+let gotopop = document.getElementsByClassName("top-btn")[0];
+window.addEventListener('scroll', showTopBtn);
+function showTopBtn() {
+    let topfar = window.pageYOffset;
+    console.log(topfar);
+if (topfar > 300) {
+    gotopop.classList.add("on");
+} else {
+    gotopop.classList.remove("on");
+}
+}
+gotop.addEventListener("click", function () {
+    document.body.scrollTop = 0; // for safari
+    document.documentElement.scrollTop = 0; // for chrom
+});
