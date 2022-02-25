39e8bd3d9ff8af6d91851b2269df6d3275827d07
Pastanks <gn02212656@gmail.com>
2022-02-26 00:49:52 +0800

    選單判斷邏輯

--- pages/contact_us/contact_us.js
+++ pages/contact_us/contact_us.js
@@ -20,3 +20,40 @@
                 }  
         });
     });
+
+
+
+
+// 選單判斷
+let datesent = document.getElementById("submitin");
+submitin.addEventListener("click", function(e){
+    e.preventDefault();
+    let date = document.getElementById("date").value;
+    let name1 = document.getElementById("name1").value;
+    let name2 = document.getElementById("name2").value;
+    let datecheck = JSON.parse(localStorage.getItem("date"));
+    let task = date;
+    let form = JSON.parse(localStorage.getItem("form"));
+       if(name1 == ""){
+       return alert("姓名未填寫");
+    }else if(name2 == ""){
+       return alert("電話未填寫");
+   }
+    if (form){ 
+       let isExisted = form.includes(date);
+        if(isExisted){
+           return alert("您已預約此日期，請重新選擇");
+        }else{
+            form.unshift(task);
+        }
+    }else{
+       form = [task];
+    }
+    localStorage.setItem("form", JSON.stringify(form)); 
+   }); 
+
+
+
+
+
+
